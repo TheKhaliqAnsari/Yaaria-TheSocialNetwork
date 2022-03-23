43e78be5335dc7f414e166b4fa14b03d6f05623c
Khaliq Ansari <khaliqansari771@gmail.com>
2022-03-24 01:44:55 +0530

    setup static files and configure express-ejs-layouts.

--- App.js
+++ App.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const port = 8888;
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
 
+//For locating static files ->
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+//extract styles and scripts from sub pages into the layout ->
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // Use express routers ->
 app.use('/', require('./routes')); 
