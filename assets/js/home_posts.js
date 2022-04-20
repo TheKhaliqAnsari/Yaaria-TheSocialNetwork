a3d7fa3bcc8f681b97b898c0b64918129d7ef9e3
Khaliq Ansari <khaliqansari771@gmail.com>
2022-04-19 18:26:06 -0700

    error code

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -1,8 +1,8 @@
-{
-    // Method to submit form data for new post using ajax
+{   
+    // method to submit the form data for new post using AJAX
     let createPost = function(){
         let newPostForm = $('#new-post-form');
-        
+
         newPostForm.submit(function(e){
             e.preventDefault();
 
@@ -11,17 +11,49 @@
                 url: '/posts/create',
                 data: newPostForm.serialize(),
                 success: function(data){
-                    console.log(data);
+                    let newPost = newPostDom(data.data.post);
+                    $('#posts-list-container>ul').prepend(newPost);
                 }, error: function(error){
                     console.log(error.responseText);
                 }
             });
-
         });
-
     }
 
+
     // method to create a post in DOM
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+                    <p>
+                        
+                        <small>
+                            <a class="delete-post-button"  href="/posts/destroy/${ post.id }">X</a>
+                        </small>
+                       
+                        ${ post.content }
+                        <br>
+                        <small>
+                        ${ post.user.name }
+                        </small>
+                    </p>
+                    <div class="post-comments">
+                        
+                            <form action="/comments/create" method="POST">
+                                <input type="text" name="content" placeholder="Type Here to add comment..." required>
+                                <input type="hidden" name="post" value="${ post._id }" >
+                                <input type="submit" value="Add Comment">
+                            </form>
+               
+                
+                        <div class="post-comments-list">
+                            <ul id="post-comments-${ post._id }">
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    
+                </li>`)
+    }
 
 
     createPost();
